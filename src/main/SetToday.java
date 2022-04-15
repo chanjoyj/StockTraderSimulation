80365968057542e459ea4520b33d2b9508a0c0cf
Anlanther <25150182+Anlanther@users.noreply.github.com>
2022-04-15 17:40:14 +0800

    setup sorting to sort int arrays ascending

--- src/main/SetToday.java
+++ src/main/SetToday.java
@@ -3,61 +3,67 @@ package main;
 public class SetToday {
 	public static int today;
 	public static int lastTradeDate;
+
 	// Change date to integer, then add a method of return to string dd/mm/yyyy
 	public static void inputDate() {
-		boolean valid=true;
+		boolean valid = true;
 		String date;
 		int today;
 		do {
 			Screen.printDateInsertion();
 			date = Screen.keyboard.nextLine();
-			valid= checkValidDate(date);
+			valid = checkValidDate(date);
 		} while (!valid);
-		today= changeDate(date);
-		today= checkDateinFile(today);
+		today = changeDate(date);
+		today = checkDateinFile(today);
 		setDate(today);
 	}
-	
+
 	public static int checkDateinFile(int date) {
-		boolean inFile =false;
+		boolean inFile = false;
 		do {
 			for (int i = 0; i < StockData.getPriceList().length; i++) {
-				if (date==StockData.getPriceList()[i].getDate()){
-					inFile= true; 
+				if (date == StockData.getPriceList()[i].getDate()) {
+					inFile = true;
 				}
 			}
 			if (!inFile) {
-				date= incrementDate(date);
+				date = incrementDate(date);
 			}
 		} while (!inFile);
 		return date;
 	}
-	
+
 	public static boolean checkValidDate(String date) {
 		String[] dateArr;
 		int today;
-			if (date.length()== 10 && !(date.indexOf("/")==-1)) {
-				dateArr= date.split("/");
-			} else return false;
+		if (date.length() == 10 && !(date.indexOf("/") == -1)) {
+			dateArr = date.split("/");
+		} else
+			return false;
 
-			dateArr[0].length();
-			if (dateArr[0].length()==2 && dateArr[1].length()==2 && dateArr[2].length()==4 &&
-					Integer.parseInt(dateArr[0])<32 && Integer.parseInt(dateArr[1])<13) {//TODO add more date requirements
-				today= changeDate(date);
-			}else return false;
-			StockData.setDateEnd(20220301);//TODO Delete later
-			StockData.setDateStart(20171201);//TODO Delete later
-			if (today > StockData.getDateEnd() || today < StockData.getDateStart() || today < lastTradeDate) {
-				return false;
-			}else return true;
+		dateArr[0].length();
+		if (dateArr[0].length() == 2 && dateArr[1].length() == 2 && dateArr[2].length() == 4 &&
+				Integer.parseInt(dateArr[0]) < 32 && Integer.parseInt(dateArr[1]) < 13) {// TODO add more date
+																							// requirements
+			today = changeDate(date);
+		} else
+			return false;
+		StockData.setDateEnd(20220301);// TODO Delete later
+		StockData.setDateStart(20171201);// TODO Delete later
+		if (today > StockData.getDateEnd() || today < StockData.getDateStart() || today < lastTradeDate) {
+			return false;
+		} else
+			return true;
 	}
-	
+
 	public static int incrementDate(int date) {
-		//TODO also check for dividends and update the other trader menus accordingly or just reload trader menu
-		//TODO change increment date to be using the date list
+		// TODO also check for dividends and update the other trader menus accordingly
+		// or just reload trader menu
+		// TODO change increment date to be using the date list
 		for (int i = 0; i < StockData.getPriceList().length; i++) {
-			if (StockData.getPriceList()[i].getDate() > date){ //assuming dates are in ascending order
-				date= StockData.getPriceList()[i].getDate();
+			if (StockData.getPriceList()[i].getDate() > date) { // assuming dates are in ascending order
+				date = StockData.getPriceList()[i].getDate();
 				break;
 			}
 		}
@@ -67,18 +73,18 @@ public class SetToday {
 	public static int getDate() {
 		return today;
 	}
-	
+
 	public static void setDate(int today2) {
-		SetToday.today= today2;
+		SetToday.today = today2;
 	}
-	
+
 	public static int changeDate(String date) {
-		return Integer.parseInt(date.substring(6)+date.substring(3,5)+ date.substring(0,2));
+		return Integer.parseInt(date.substring(6) + date.substring(3, 5) + date.substring(0, 2));
 	}
-	
+
 	public static String revertDate(int date) {
-		String dates= date+"";
-		return dates.substring(6)+"/"+dates.substring(4,6)+"/"+dates.substring(0,4);
+		String dates = date + "";
+		return dates.substring(6) + "/" + dates.substring(4, 6) + "/" + dates.substring(0, 4);
 	}
-	
+
 }
--- src/main/StockTrader.java
+++ src/main/StockTrader.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.io.FileNotFoundException;
+import java.util.Random;
+
 import menuController.MenuController;
 
 //Main runner
@@ -11,5 +13,28 @@ public class StockTrader {
 		System.out.println();
 		SetToday.inputDate();
 		MenuController.startMenu();
+
+		// ===== For testing Sorting =====
+		// Random rand = new Random();
+		// int[] numbers = new int[10];
+
+		// for (int i = 0; i < numbers.length; i++) {
+		// 	numbers[i] = rand.nextInt(100);
+		// }
+
+		// System.out.println("Before:");
+		// printArray(numbers);
+
+		// Sorting.quickSort(numbers, 0, numbers.length-1);
+
+		// System.out.println("\nAfter:");
+		// printArray(numbers);
 	}
+
+	// ===== For testing Sorting =====
+	// private static void printArray(int[] numbers) {
+	// 	for (int i = 0; i < numbers.length; i++) {
+	// 	  System.out.println(numbers[i]);
+	// 	}
+	//   }
 }
